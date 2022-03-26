accd97c1cae1540dd4777ab0f7989b1a735a69ef
Junichi Kato <j5ik2o@gmail.com>
2022-03-26 21:36:07 +0900

    add files

--- src/main/scala/s/template_method/Main.scala
+++ src/main/scala/s/template_method/Main.scala
@@ -2,7 +2,7 @@
 package s.template_method
 
 object Main {
-  def main(args: Array[String]): Unit = { // 'H'を持ったCharDisplayのインスタンスを1個作る
+  def main(args: Array[String]): Unit = {
     val d1: AbstractDisplay = new CharDisplay('H')
     val d2: AbstractDisplay = new StringDisplay("Hello, world.")
 
