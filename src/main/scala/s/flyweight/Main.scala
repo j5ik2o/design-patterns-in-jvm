9a2321a1d755838ab0c8950d5ff0468174c798e0
Junichi Kato <j5ik2o@gmail.com>
2022-03-27 00:54:50 +0900

    wip

--- src/main/scala/s/flyweight/Main.scala
+++ src/main/scala/s/flyweight/Main.scala
@@ -3,14 +3,12 @@ package s.flyweight
 
 import j.flyweight.BigString
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    if (args.isEmpty) {
-      println("Usage: java Main digits")
-      println("Example: java Main 1212123")
-      System.exit(0)
-    }
-    val bs = new BigString(args(0))
-    bs.print()
+object Main extends App {
+  if (args.isEmpty) {
+    println("Usage: java Main digits")
+    println("Example: java Main 1212123")
+    System.exit(0)
   }
+  val bs = new BigString(args(0))
+  bs.print()
 }
