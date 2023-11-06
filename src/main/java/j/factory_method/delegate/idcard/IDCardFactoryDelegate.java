8d9f43cf029249ef1e2a7ce60078a795018d79a5
Junichi Kato <j5ik2o@gmail.com>
2023-11-06 12:17:04 +0900

    add impls.

---
+++ src/main/java/j/factory_method/delegate/idcard/IDCardFactoryDelegate.java
@@ -0,0 +1,16 @@
+package j.factory_method.delegate.idcard;
+
+import j.factory_method.delegate.framework.FactoryDelegate;
+import j.factory_method.delegate.framework.Product;
+
+public class IDCardFactoryDelegate implements FactoryDelegate {
+    @Override
+    public Product createProduct(String owner) {
+        return new IDCard(owner);
+    }
+
+    @Override
+    public void registerProduct(Product product) {
+        System.out.println(product + "を登録します。");
+    }
+}
