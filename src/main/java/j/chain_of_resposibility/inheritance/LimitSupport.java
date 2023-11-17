c279693b489d9536a37afa99ba37deb8076cb1ed
Junichi Kato <j5ik2o@gmail.com>
2023-11-18 01:58:31 +0900

    wip

--- src/main/java/j/chain_of_resposibility/inheritance/LimitSupport.java
+++ src/main/java/j/chain_of_resposibility/inheritance/LimitSupport.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public final class LimitSupport extends Support {
-    private int limit; // この番号未満なら解決できる
+    private final int limit; // この番号未満なら解決できる
 
     public LimitSupport(@NotNull String name, int limit, @Nullable Support next) {
         super(name, next);
