8be397f9ec5e06b9318617e1f1399647634eea5c
Junichi Kato <j5ik2o@gmail.com>
2023-11-17 20:25:00 +0900

    refactor

--- src/main/java/j/chain_of_resposibility/delegate/SupportFactory.java
+++ src/main/java/j/chain_of_resposibility/delegate/SupportFactory.java
@@ -3,15 +3,15 @@ package j.chain_of_resposibility.delegate;
 import org.jetbrains.annotations.NotNull;
 
 public class SupportFactory {
-    public static Support createNoSupport(@NotNull String name, SupportDelegate next) {
+    public static Support createNoSupport(@NotNull String name, Support next) {
         return new NoSupport(name, next);
     }
 
-    public static Support createLimitSupport(@NotNull String name, SupportDelegate next, int limit) {
+    public static Support createLimitSupport(@NotNull String name, Support next, int limit) {
         return new LimitSupport(name, next, limit);
     }
 
-    public static Support createOddSupport(@NotNull String name, SupportDelegate next) {
+    public static Support createOddSupport(@NotNull String name, Support next) {
         return new OddSupport(name, next);
     }
 }
