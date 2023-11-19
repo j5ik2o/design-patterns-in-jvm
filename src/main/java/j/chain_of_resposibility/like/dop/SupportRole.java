a734838e112771c3c340b954f74241d73ac79544
Junichi Kato <j5ik2o@gmail.com>
2023-11-19 15:27:27 +0900

    wip

--- src/main/java/j/chain_of_resposibility/like/dop/SupportRole.java
+++ src/main/java/j/chain_of_resposibility/like/dop/SupportRole.java
@@ -3,28 +3,28 @@ package j.chain_of_resposibility.like.dop;
 import j.chain_of_resposibility.Trouble;
 
 public final class SupportRole {
-    public static void support(SupportData self, Trouble trouble) {
-        if (resolve(self, trouble)) {
-            done(self, trouble);
-        } else if (self.next() != null) {
-            support(self.next(), trouble);
+  public static void support(SupportData self, Trouble trouble) {
+    if (resolve(self, trouble)) {
+      done(self, trouble);
+    } else if (self.next() != null) {
+      support(self.next(), trouble);
     } else {
       fail(trouble);
     }
   }
 
-    private static boolean resolve(SupportData self, Trouble trouble) {
-        return switch (self) {
-            case SupportData.NoSupportData ignore -> false;
-            case SupportData.OddSupportData ignore -> trouble.getNumber() % 2 == 1;
-            case SupportData.LimitSupportData limitSupport -> trouble.getNumber() < limitSupport.limit();
-            case SupportData.SpecialSupportData specialSupport -> trouble.getNumber()
-                    == specialSupport.number();
+  private static boolean resolve(SupportData self, Trouble trouble) {
+    return switch (self) {
+      case SupportData.NoSupportData ignore -> false;
+      case SupportData.OddSupportData ignore -> trouble.getNumber() % 2 == 1;
+      case SupportData.LimitSupportData limitSupport -> trouble.getNumber() < limitSupport.limit();
+      case SupportData.SpecialSupportData specialSupport -> trouble.getNumber()
+          == specialSupport.number();
     };
   }
 
-    private static void done(SupportData self, Trouble trouble) {
-        System.out.println(trouble + " is resolved by [" + self.name() + "].");
+  private static void done(SupportData self, Trouble trouble) {
+    System.out.println(trouble + " is resolved by [" + self.name() + "].");
   }
 
   private static void fail(Trouble trouble) {
